dade647f83925f1a89061ee7146e340aec83ac46
dailch <hoangdai9895@gmail.com>
2020-11-16 21:30:14 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import OnBoarding from "./app/screens/OnBoarding/OnBoarding";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="OnBoarding"
+					component={OnBoarding}
+					options={{ headerShown: false }}></Stack.Screen>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+export default () => {
+	return <App />;
+};
